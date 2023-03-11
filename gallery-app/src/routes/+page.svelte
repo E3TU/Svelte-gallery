b8946a783bdebaf14bf9910cb4698c3e41cf683c
E3TU <99178022+E3TU@users.noreply.github.com>
2023-03-11 16:38:52 +0200

    Create svelte app

---
+++ gallery-app/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    let test = "hello world";
+</script>
+
+<div class="container">
+    <div class="textcontainer">
+        <h1>Svelte Gallery App</h1>
+        <input>
+    </div>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        width: 100%;
+        height: auto;
+        justify-content: center;
+    }
+    .textcontainer{
+        text-align: center;
+        width: 20rem;
+    }
+</style>
