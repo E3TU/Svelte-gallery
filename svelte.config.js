bd10b9a8a5d9b42102bd4b088ce76dd43c126448
E3TU <99178022+E3TU@users.noreply.github.com>
2023-03-14 22:29:47 +0200

    Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,29 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
 
-// export default config;
-import adapter from '@sveltejs/adapter-static';
-
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-	  adapter: adapter({
-		// default options are shown. On some platforms
-		// these options are set automatically — see below
-		pages: 'build',
-		assets: 'build',
-		fallback: null,
-		precompress: false,
-		strict: true
-	  })
-	}
-  };
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	},
+};
+
+export default config;
+// import adapter from '@sveltejs/adapter-static';
+
+// export default {
+// 	kit: {
+// 	  adapter: adapter({
+// 		// default options are shown. On some platforms
+// 		// these options are set automatically — see below
+// 		pages: 'build',
+// 		assets: 'build',
+// 		fallback: null,
+// 		precompress: false,
+// 		strict: true
+// 	  })
+// 	}
+//   };
